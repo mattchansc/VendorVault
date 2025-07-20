c49bb53616d42e8e4a6fd0d10cb66274fbc437c9
Matthew Chan <mattchansc@gmail.com>
2025-07-20 13:35:10 -0400

    iOS starter code

---
+++ VendorVault/VendorVault/VendorVaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  VendorVaultApp.swift
+//  VendorVault
+//
+//  Created by Matthew Chan on 7/20/25.
+//
+
+import SwiftUI
+
+@main
+struct VendorVaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VendorVault/VendorVaultTests/VendorVaultTests.swift
@@ -0,0 +1,16 @@
+//
+//  VendorVaultTests.swift
+//  VendorVaultTests
+//
+//  Created by Matthew Chan on 7/20/25.
+//
+
+import Testing
+
+struct VendorVaultTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
